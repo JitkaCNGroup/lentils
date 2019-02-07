9aecca5253a07a7ac1376553de157216c2a0ace4
Jitka Pemcakova <pemcakova@cngroup.dk>
2019-02-07 16:15:33 +0100

    CypherServiceRest - Mock tests added

--- src/test/java/dk/cngroup/lentils/service/CypherServiceTest.java
+++ src/test/java/dk/cngroup/lentils/service/CypherServiceTest.java
@@ -2,56 +2,57 @@ package dk.cngroup.lentils.service;
 
 import dk.cngroup.lentils.LentilsApplication;
 import dk.cngroup.lentils.entity.Cypher;
+import dk.cngroup.lentils.repository.CypherRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 @SpringBootTest(classes = {LentilsApplication.class})
 public class CypherServiceTest
 {
-    //@InjectMocks
-    @Autowired
-    CypherService service;
-
-    private static final Integer TESTED_STAGE = 4;
+    private static final Integer TESTED_STAGE = 3;
     private static final Integer NUM_OF_ALL_CYPHERS = 5;
     private static final String CODEWORD = "Codeword";
     private static final String CODEWORD_FALSE = "CodewordFalse";
 
-    //@Mock
-   // @Autowired
-    //CypherRepository repository;
-/*
+    @InjectMocks
+    //@Autowired
+    CypherService service;
+
+    @Mock
+    CypherRepository repository;
+
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
     }
-
-*/
-    @Before
-    public void setUp() {
-        service.deleteAllCyphers();
-    }
+    //public void setUp() {        service.deleteAllCyphers();    }
 
     private Cypher getCypherForStage(Integer stage)
     {
-        return service.saveNewCypher(new Cypher("Easy", stage, 49.0988161, 17.7519189, CODEWORD, "hledej dole"));
+        Cypher cypher = new Cypher("Easy", stage, 49.0988161, 17.7519189, CODEWORD, "hledej dole");
+        when(repository.save(cypher)).thenReturn(cypher);
+        return cypher;
+        //return service.saveNewCypher(new Cypher("Easy", stage, 49.0988161, 17.7519189, CODEWORD, "hledej dole"));
     }
 
     @Test
     public void addNewCypher()
     {
-        //Cypher actualCypher = repository.save(newCypher);
-        //when(repository.findByStage(3)).thenReturn(newCypher);
-
         Integer id  = service.addNewCypher(getCypherForStage(TESTED_STAGE));
 
         assertNotNull(id);
@@ -62,6 +63,7 @@ public class CypherServiceTest
     {
         Cypher cypher = getCypherForStage(TESTED_STAGE);
 
+        when(repository.findByStage(TESTED_STAGE)).thenReturn(cypher);
         String hint = service.getHintForStage(TESTED_STAGE);
 
         assertEquals (cypher.getHint(), hint);
@@ -73,6 +75,8 @@ public class CypherServiceTest
         Cypher cypher1 = getCypherForStage(TESTED_STAGE);
         Cypher cypher2 = getCypherForStage(TESTED_STAGE + 1);
 
+        when(repository.findByStage(TESTED_STAGE)).thenReturn(cypher1);
+        when(repository.findByStage(TESTED_STAGE + 1)).thenReturn(cypher2);
         Cypher cypher = service.getNextCypher(TESTED_STAGE);
 
         assertEquals(cypher2, cypher);
@@ -81,7 +85,9 @@ public class CypherServiceTest
     @Test
     public void checkCodeword()
     {
-        getCypherForStage(TESTED_STAGE);
+        Cypher cypher = getCypherForStage(TESTED_STAGE);
+
+        when(repository.findByStage(TESTED_STAGE)).thenReturn(cypher);
 
         assertTrue(service.checkCodeword(CODEWORD, TESTED_STAGE));
         assertFalse(service.checkCodeword(CODEWORD_FALSE, TESTED_STAGE));
