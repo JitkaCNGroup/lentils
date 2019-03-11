97b228964f197674cecb7b1d4d936657e7683dc2
Radek Crlík <nix0@centrum.cz>
2019-03-11 18:47:14 +0100

    Remove unused import in Status Repository

--- src/main/java/dk/cngroup/lentils/repository/StatusRepository.java
+++ src/main/java/dk/cngroup/lentils/repository/StatusRepository.java
@@ -5,7 +5,6 @@ import dk.cngroup.lentils.entity.Status;
 import dk.cngroup.lentils.entity.StatusKey;
 import dk.cngroup.lentils.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
