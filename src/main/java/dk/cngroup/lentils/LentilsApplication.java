aa6bb7724a492cdc34b9f965f91440c89f64d140
Radek Crlík <crlik@cngroup.dk>
2019-03-11 17:02:12 +0100

    Marked args param in main method as final

--- src/main/java/dk/cngroup/lentils/LentilsApplication.java
+++ src/main/java/dk/cngroup/lentils/LentilsApplication.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 public class LentilsApplication {
-
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(LentilsApplication.class, args);
         System.out.print("Runs, see db at localhost:8080/h2-console");
     }
