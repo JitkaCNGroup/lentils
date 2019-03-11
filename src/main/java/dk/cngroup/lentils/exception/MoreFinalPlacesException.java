f78cdfffc0d3079d6bf9461ac6ca5c1bbb0f9e75
Radek Crlík <crlik@cngroup.dk>
2019-03-11 16:34:51 +0100

    Refactor MoreFinalPlacesException to comply with checkstyle

--- src/main/java/dk/cngroup/lentils/exception/MoreFinalPlacesException.java
+++ src/main/java/dk/cngroup/lentils/exception/MoreFinalPlacesException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-public class MoreFinalPlacesException extends RuntimeException{
-    public MoreFinalPlacesException(String msg) {
-        super(String.format(msg));
+public class MoreFinalPlacesException extends RuntimeException {
+    public MoreFinalPlacesException(final String msg) {
+        super(msg);
     }
 }
