0fa778abe4358b3892411e1dfcf9b37760367634
Radek Crlík <crlik@cngroup.dk>
2019-03-12 15:22:58 +0100

    Refactor HintTakeKey to comply with checkstyle

--- src/main/java/dk/cngroup/lentils/entity/HintTakenKey.java
+++ src/main/java/dk/cngroup/lentils/entity/HintTakenKey.java
@@ -1,8 +1,43 @@
 package dk.cngroup.lentils.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class HintTakenKey implements Serializable {
-    Long team;
-    Long hint;
+    private Long team;
+    private Long hint;
+
+    public HintTakenKey() {
+    }
+
+    public HintTakenKey(final Long teamId, final Long hintId) {
+        this.team = teamId;
+        this.hint = hintId;
+    }
+
+    public Long getTeam() {
+        return team;
+    }
+
+    public Long getHint() {
+        return hint;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        HintTakenKey that = (HintTakenKey) o;
+        return Objects.equals(team, that.team) &&
+                Objects.equals(hint, that.hint);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(team, hint);
+    }
 }
