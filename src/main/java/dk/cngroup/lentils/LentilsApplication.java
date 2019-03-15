58e4e97e8282101d1abac6e647a4e78c32932077
Radek Crlík <crlik@cngroup.dk>
2019-03-15 10:51:38 +0100

    #55 Support for WAR build output

--- src/main/java/dk/cngroup/lentils/LentilsApplication.java
+++ src/main/java/dk/cngroup/lentils/LentilsApplication.java
@@ -2,11 +2,18 @@ package dk.cngroup.lentils;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class LentilsApplication {
+public class LentilsApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(LentilsApplication.class);
+    }
+
     public static void main(final String[] args) {
         SpringApplication.run(LentilsApplication.class, args);
         System.out.print("Runs, see db at localhost:8080/h2-console");
