25c93157dc7ed959fc44ea175744d20d0fdc3494
kostka <dkostka@eurofunk.com>
2019-03-19 19:50:49 +0100

    pridano dependency a prvni user a login page
    security - loginController + enable csrf

---
+++ src/main/java/dk/cngroup/lentils/security/WebSecurityConfiguration.java
@@ -0,0 +1,40 @@
+package dk.cngroup.lentils.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(getPasswordEncoder())
+                .withUser("admin")
+                .password(getPasswordEncoder().encode("admin"))
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/team").permitAll()
+                    .anyRequest().hasRole("USER").and()
+                    .formLogin()
+                        .loginPage("/login")
+                        .permitAll()
+                        .and()
+                    .logout()
+                        .logoutSuccessUrl("/login")
+                        .permitAll();
+    }
+}
