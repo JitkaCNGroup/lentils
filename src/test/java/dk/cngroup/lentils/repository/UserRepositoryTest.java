17c24aae6b801c612afdca1a59207f3f9a707035
kostka <dkostka@eurofunk.com>
2019-03-27 13:09:32 +0100

    rozdelena metoda na vytvoreni user, team a vyreseny review komentare

--- src/test/java/dk/cngroup/lentils/repository/UserRepositoryTest.java
+++ src/test/java/dk/cngroup/lentils/repository/UserRepositoryTest.java
@@ -3,7 +3,6 @@ package dk.cngroup.lentils.repository;
 import dk.cngroup.lentils.LentilsApplication;
 import dk.cngroup.lentils.entity.Team;
 import dk.cngroup.lentils.entity.User;
-import dk.cngroup.lentils.service.ObjectGenerator;
 import dk.cngroup.lentils.service.TeamService;
 import org.junit.Assert;
 import org.junit.Test;
@@ -17,8 +16,13 @@ import java.util.List;
 
 @Transactional
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {LentilsApplication.class, ObjectGenerator.class})
+@SpringBootTest(classes = {LentilsApplication.class})
 public class UserRepositoryTest {
+    private static final String TESTED_TEAM_NAME = "Team42";
+    private static final String TEAM_PIN = "1234";
+    private static final String TEST_USERNAME = "TestedUsername";
+    private static final String TEST_PASSWORD = "SecretPassword";
+
     @Autowired
     TeamService teamService;
 
@@ -28,9 +32,9 @@ public class UserRepositoryTest {
     @Test
     public void saveTeamCreateNewUser() {
         long count = userRepository.count();
-        Team team = new Team("teamA", 5, "1234");
-        User user = new User();
-        addValidUserToTeam(team, user);
+        Team team = createValidTeam();
+        User user = createValidUser();
+        addUserToTeam(team, user);
         teamService.save(team);
         Assert.assertEquals(count + 1, userRepository.count());
     }
@@ -39,14 +43,14 @@ public class UserRepositoryTest {
     public void adminUserIsImportedOnStartup() {
         Assert.assertEquals(1, userRepository.count());
         List<User> users = userRepository.findAll();
-        Assert.assertEquals("admin",users.get(0).getUsername());
+        Assert.assertEquals("admin", users.get(0).getUsername());
     }
 
     @Test
     public void updateTeamDoNotCreateNewUser() {
-        Team team = new Team("teamA", 5, "1234");
-        User user = new User();
-        addValidUserToTeam(team, user);
+        Team team = createValidTeam();
+        User user = createValidUser();
+        addUserToTeam(team, user);
         teamService.save(team);
         long count = userRepository.count();
         teamService.update(team);
@@ -55,18 +59,27 @@ public class UserRepositoryTest {
 
     @Test
     public void deleteTeamRemoveUser() {
-        Team team = new Team("teamA", 5, "1234");
-        User user = new User();
-        addValidUserToTeam(team, user);
+        Team team = createValidTeam();
+        User user = createValidUser();
+        addUserToTeam(team, user);
         teamService.save(team);
         long count = userRepository.count();
         teamService.delete(team.getTeamId());
         Assert.assertEquals(count - 1, userRepository.count());
     }
 
-    private void addValidUserToTeam(Team team, User user) {
-        user.setUsername("testUsername");
-        user.setPassword("pwd");
+    private Team createValidTeam() {
+        return new Team(TESTED_TEAM_NAME, 5, TEAM_PIN);
+    }
+
+    private User createValidUser() {
+        User user = new User();
+        user.setUsername(TEST_USERNAME);
+        user.setPassword(TEST_PASSWORD);
+        return user;
+    }
+
+    private void addUserToTeam(Team team, User user) {
         team.setUser(user);
     }
 }
