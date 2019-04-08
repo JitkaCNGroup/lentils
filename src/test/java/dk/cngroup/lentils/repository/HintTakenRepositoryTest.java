56ebf2390af5c348120ef495467460e93dd22287
Pavel Jurca <jurca@cngroup.dk>
2019-04-08 13:26:54 +0200

    delete unused import

--- src/test/java/dk/cngroup/lentils/repository/HintTakenRepositoryTest.java
+++ src/test/java/dk/cngroup/lentils/repository/HintTakenRepositoryTest.java
@@ -21,7 +21,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 @RunWith(SpringRunner.class)
 @Transactional
