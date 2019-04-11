b8787781b9da0987b52ab945485eb66a308cfb9f
kostka <dkostka@eurofunk.com>
2019-04-11 08:39:51 +0200

    checkstyle fix

--- src/main/java/dk/cngroup/lentils/util/UsernameUtils.java
+++ src/main/java/dk/cngroup/lentils/util/UsernameUtils.java
@@ -5,11 +5,12 @@ import java.text.Normalizer;
 public class UsernameUtils {
 
     public static String generateUsername(final String oldUsername) {
+        final int allowedWords = 4;
         String newUsername = Normalizer
                 .normalize(oldUsername, Normalizer.Form.NFD)
                 .toLowerCase()
                 .replaceAll("[^\\p{ASCII}]", "")
-                .replaceAll("^((?:\\W*\\w+){" + 4 + "}).*$", "$1")
+                .replaceAll("^((?:\\W*\\w+){" + allowedWords + "}).*$", "$1")
                 .replace(" ", "_");
         return newUsername;
     }
