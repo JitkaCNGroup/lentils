413e6aace818ed52f14dc008e06be8e666678b4c
Pavel Jurca <jurca@cngroup.dk>
2019-04-15 17:12:37 +0200

    delete unused import

--- src/main/java/dk/cngroup/lentils/repository/HintRepository.java
+++ src/main/java/dk/cngroup/lentils/repository/HintRepository.java
@@ -3,7 +3,6 @@ package dk.cngroup.lentils.repository;
 import dk.cngroup.lentils.entity.Cypher;
 import dk.cngroup.lentils.entity.Hint;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
