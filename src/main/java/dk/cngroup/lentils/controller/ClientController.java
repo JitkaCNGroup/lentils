64de683b64696a05f7a39dce7125887356e14bf4
Pavel Jurca <jurca@cngroup.dk>
2019-04-29 16:00:00 +0200

    implement FinalPlace opening time for client

--- src/main/java/dk/cngroup/lentils/controller/ClientController.java
+++ src/main/java/dk/cngroup/lentils/controller/ClientController.java
@@ -78,6 +78,11 @@ public class ClientController {
         } else {
             model.addAttribute("gameStarted", false);
         }
+        if (!gameLogicService.allowPlayersToViewFinalPlace(user.getTeam())) {
+            model.addAttribute("finalViewNotAllowed", true);
+        } else {
+            model.addAttribute("openingTime", gameLogicService.getFinalPlaceOpeningTime());
+        }
         model.addAttribute("team", user.getTeam());
         return CLIENT_VIEW_CYPHER_LIST;
     }
