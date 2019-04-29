bc8ba156b96f607864cc5148e0fec51317f79fde
Pavel Jurca <jurca@cngroup.dk>
2019-04-29 09:50:26 +0200

    del unused import

--- src/main/java/dk/cngroup/lentils/service/FinalPlaceService.java
+++ src/main/java/dk/cngroup/lentils/service/FinalPlaceService.java
@@ -6,7 +6,6 @@ import dk.cngroup.lentils.repository.FinalPlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
