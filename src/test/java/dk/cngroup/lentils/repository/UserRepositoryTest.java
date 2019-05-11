418e023cb85b986c2a8ee9563ecccf4cd8e23e9c
Martin Janousek <mjanousek92@gmail.com>
2019-05-11 10:50:59 +0200

    Unused imports removed

--- src/test/java/dk/cngroup/lentils/repository/UserRepositoryTest.java
+++ src/test/java/dk/cngroup/lentils/repository/UserRepositoryTest.java
@@ -4,7 +4,6 @@ import dk.cngroup.lentils.LentilsApplication;
 import dk.cngroup.lentils.entity.Team;
 import dk.cngroup.lentils.entity.User;
 import dk.cngroup.lentils.service.TeamService;
-import dk.cngroup.lentils.util.UsernameUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.text.Normalizer;
 import java.util.List;
 import java.util.Optional;
 
