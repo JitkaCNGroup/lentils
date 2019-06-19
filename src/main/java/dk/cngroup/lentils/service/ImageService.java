79b16b0ba26b1c94cd97cc31f960d24f27836994
Jana Sedlackova <sedlackova@cngroup.dk>
2019-06-19 14:12:10 +0200

    reading and saving image file, tests

--- src/main/java/dk/cngroup/lentils/service/ImageService.java
+++ src/main/java/dk/cngroup/lentils/service/ImageService.java
@@ -1,11 +1,11 @@
 package dk.cngroup.lentils.service;
 
-import dk.cngroup.lentils.util.TextChooser;
 import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
-import java.awt.FileDialog;
-import java.awt.Frame;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -13,26 +13,46 @@ import java.io.IOException;
 @Service
 public class ImageService {
 
-    public File chooseFileWithFileDialog() {
-        FileDialog dialog = new FileDialog((Frame) null, "Select bmp, png or jpg File with to Open");
-        dialog.setMode(FileDialog.LOAD);
-        dialog.setFilenameFilter(new TextChooser());
-        dialog.setVisible(true);
-        String dir = dialog.getDirectory();
-        String file = dialog.getFile();
-        if (dir == null || file == null) {
-            return null;
+    public static final String IMAGE_DIRECTORY = "\\images\\";
+
+    public static File chooseFile() {
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        jfc.setDialogTitle("Select an image");
+        jfc.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "PNG, JPG, JPEG, BMP and GIF images", "png", "jpg", "jpeg", "bmp", "gif");
+        jfc.addChoosableFileFilter(filter);
+
+        int returnValue = jfc.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            return jfc.getSelectedFile();
         }
-        return new File(dir, file);
+        return null;
     }
 
-    public void openImageFile(final String fileName) {
+    public static void readAndSaveImageFile(final File file) {
+        BufferedImage image = null;
         try {
-            BufferedImage myPicture = ImageIO.read(new File(fileName));
-            ImageIO.write(myPicture, "BMP", new File("filename.bmp"));
+            image = ImageIO.read(file);
         } catch (IOException e) {
             System.err.println("Picture file cannot be open. " + e.getMessage());
         }
+        String fileName = file.getName();
+        String pathToSave = getDirectory() + fileName;
+        String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1);
+        try {
+            ImageIO.write(image, fileExtension, new File(pathToSave));
+        } catch (IOException e) {
+            System.err.println("Picture file cannot be saved. " + e.getMessage());
+        }
+    }
 
+    private static String getDirectory() {
+        String newDirectory = System.getProperty("user.dir") + IMAGE_DIRECTORY;
+        File newDirectoryDir = new File(newDirectory);
+        if (!newDirectoryDir.exists()) {
+            newDirectoryDir.mkdir();
+        }
+        return newDirectory;
     }
 }
--- src/test/java/dk/cngroup/lentils/service/ImageServiceTest.java
+++ src/test/java/dk/cngroup/lentils/service/ImageServiceTest.java
@@ -1,20 +1,19 @@
 package dk.cngroup.lentils.service;
 
-import dk.cngroup.lentils.entity.Cypher;
-import dk.cngroup.lentils.entity.Team;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.List;
+import java.io.File;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertTrue;
 
 public class ImageServiceTest {
+
+    static final String JPG_FILE_TO_OPEN = "\\src\\main\\resources\\static\\img\\billy.jpg";
+    static final String JPG_FILE_NAME = "\\billy.jpg";
 
     @InjectMocks
     ImageService imageService;
@@ -24,10 +23,29 @@ public class ImageServiceTest {
         MockitoAnnotations.initMocks(this);
     }
 
+    //file to choose - "user.home" + \\src\\main\\resources\\static\\img\\billy.jpg"
     @Test
-    public void chooseFilewithFileDialog() {
-        assertEquals("hintcard", imageService.chooseFileWithFileDialog());
+    public void chooseJpgFile() {
+        String fileToOpen = System.getProperty("user.dir") + JPG_FILE_TO_OPEN;
+        assertEquals(fileToOpen, ImageService.chooseFile().toString());
     }
 
+    //cancel choosing file
+    @Test(expected = NullPointerException.class)
+    public void chooseFileCanceled() {
+        ImageService.chooseFile().toString();
+    }
+
+    //file to choose - "user.home" + \\src\\main\\resources\\static\\img\\billy.jpg"
+    @Test
+    public void chooseAndReadAndSaveImageWhenDirecotoryNotExists() {
+        File file = ImageService.chooseFile();
+        String path = System.getProperty("user.dir") + ImageService.IMAGE_DIRECTORY;
+        File newDirectory = new File(path);
+        File newFile = new File(newDirectory + JPG_FILE_NAME);
+        ImageService.readAndSaveImageFile(file);
+        assertTrue(newDirectory.exists());
+        assertTrue(newFile.exists());
+    }
 
 }
