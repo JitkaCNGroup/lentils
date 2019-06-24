dc9558b99945f2fe51834ad1a07e79a2661ce766
Pavel Jurca <jurca@cngroup.dk>
2019-06-24 17:07:44 +0200

    fix deleteById method

--- src/main/java/dk/cngroup/lentils/service/CypherService.java
+++ src/main/java/dk/cngroup/lentils/service/CypherService.java
@@ -16,10 +16,12 @@ import java.util.Optional;
 public class CypherService {
     private final CypherRepository cypherRepository;
     private StatusService statusService;
+    private HintService hintService;
 
     @Autowired
-    public void setStatusService(final StatusService statusService) {
+    public void setStatusService(final StatusService statusService, final HintService hintService) {
         this.statusService = statusService;
+        this.hintService = hintService;
     }
 
     @Autowired
@@ -76,6 +78,8 @@ public class CypherService {
 
     @Transactional
     public void deleteById(final Long id) {
+        getCypher(id).getHints().stream()
+                .forEach(hint -> hintService.deleteById(hint.getHintId()));
         statusService.deleteAllByCypherId(id);
         cypherRepository.deleteById(id);
     }
