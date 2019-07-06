5a830c92e34585e3846fbdda2d67a3436bf2e7b6
Radek Crlík <crlik@cngroup.dk>
2019-07-06 11:00:09 +0200

    Add private constructor for utility classes

--- src/main/java/dk/cngroup/lentils/util/CzechCharsetUtils.java
+++ src/main/java/dk/cngroup/lentils/util/CzechCharsetUtils.java
@@ -2,7 +2,12 @@ package dk.cngroup.lentils.util;
 
 import java.text.Normalizer;
 
-public class CzechCharsetUtils {
+public final class CzechCharsetUtils {
+
+    private CzechCharsetUtils() {
+        throw new IllegalStateException("Utility class cannot be instantiated");
+    }
+
     public static String replaceCzechSpecialCharacters(final String oldUsername) {
         return Normalizer
                 .normalize(oldUsername, Normalizer.Form.NFD)
--- src/main/java/dk/cngroup/lentils/util/LoginUtils.java
+++ src/main/java/dk/cngroup/lentils/util/LoginUtils.java
@@ -2,7 +2,11 @@ package dk.cngroup.lentils.util;
 
 import java.util.Collection;
 
-public class LoginUtils {
+public final class LoginUtils {
+
+    private LoginUtils() {
+        throw new IllegalStateException("Utility class cannot be instantiated");
+    }
 
     public static String getRedirectUrlAfterLoginForRole(final Collection role) {
         switch (role.toString()) {
