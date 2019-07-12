0741c7db53f48348b53f2bd0400a897c8d2962c4
Pavel Jurca <jurca@cngroup.dk>
2019-07-12 14:22:40 +0200

    ScoreItem override equals

--- src/main/java/dk/cngroup/lentils/entity/core/ScoreItem.java
+++ src/main/java/dk/cngroup/lentils/entity/core/ScoreItem.java
@@ -3,6 +3,8 @@ package dk.cngroup.lentils.entity.core;
 import dk.cngroup.lentils.entity.view.Rank;
 import dk.cngroup.lentils.entity.view.TeamScore;
 
+import java.util.Objects;
+
 public class ScoreItem implements Comparable {
     private int score;
     private Rank rank;
@@ -27,5 +29,23 @@ public class ScoreItem implements Comparable {
     public int compareTo(final Object o) {
         int compareScore = ((TeamScore) o).getScore();
         return compareScore - getScore();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ScoreItem)) {
+            return false;
+        }
+        ScoreItem scoreItem = (ScoreItem) o;
+        return score == scoreItem.score &&
+                Objects.equals(rank, scoreItem.rank);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, rank);
     }
 }
