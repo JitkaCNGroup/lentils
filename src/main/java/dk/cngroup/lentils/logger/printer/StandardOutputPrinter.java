e09a1472ea0f549dcc190724754395e2de24f99e
Radek Crlík <crlik@cngroup.dk>
2019-07-20 11:56:37 +0200

    Refactor Logger

--- src/main/java/dk/cngroup/lentils/logger/printer/StandardOutputPrinter.java
+++ src/main/java/dk/cngroup/lentils/logger/printer/StandardOutputPrinter.java
@@ -2,10 +2,12 @@ package dk.cngroup.lentils.logger.printer;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class StandardOutputPrinter extends OutputPrinter {
 
-    @Override
+    @PostConstruct
     public void initOutput() {
         setOutput(System.out);
     }
