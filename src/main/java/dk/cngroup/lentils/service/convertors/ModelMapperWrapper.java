08f2da58ab6fc0cc1b9545a67b51a76909647f40
Pavel Jurca <jurca@cngroup.dk>
2019-07-23 10:26:45 +0200

    add cypher-organizer mapping

--- src/main/java/dk/cngroup/lentils/service/convertors/ModelMapperWrapper.java
+++ src/main/java/dk/cngroup/lentils/service/convertors/ModelMapperWrapper.java
@@ -1,10 +1,27 @@
 package dk.cngroup.lentils.service.convertors;
 
+import dk.cngroup.lentils.dto.CypherFormDTO;
+import dk.cngroup.lentils.entity.Cypher;
+import dk.cngroup.lentils.entity.User;
+import dk.cngroup.lentils.service.UserService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ModelMapperWrapper implements ObjectMapper {
+
+    private UserService userService;
+
+    @Autowired
+    public ModelMapperWrapper(final UserService userService) {
+        this.userService = userService;
+    }
+
     private final ModelMapper modelMapper = new ModelMapper();
 
     @Override
@@ -15,5 +32,32 @@ public class ModelMapperWrapper implements ObjectMapper {
     @Override
     public <T> T map(final Object source, final Class<T> destinationClassName) {
         return modelMapper.map(source, destinationClassName);
+    }
+
+    public Cypher map(final CypherFormDTO source) {
+        Cypher cypher = map(source, Cypher.class);
+        cypher.setOrganizers(userService.getOrganizersByIds(source.getOrganizers()));
+        return cypher;
+    }
+
+    public CypherFormDTO map(final Cypher source) {
+        skipOrganizersMappingToDTO();
+        CypherFormDTO cypherFormDto = modelMapper.map(source, CypherFormDTO.class);
+        cypherFormDto.setOrganizers(getOrganizerIds(source.getOrganizers()));
+        return cypherFormDto;
+    }
+
+    private List<Long> getOrganizerIds(final List<User> organizers) {
+        return organizers.stream()
+                .map(User::getUserId)
+                .collect(Collectors.toList());
+    }
+
+    private void skipOrganizersMappingToDTO() {
+        modelMapper.addMappings(new PropertyMap<Cypher, CypherFormDTO>() {
+            protected void configure() {
+                skip().setOrganizers(null);
+            }
+        });
     }
 }
--- src/test/java/dk/cngroup/lentils/service/ModelMapperWrapperTest.java
+++ src/test/java/dk/cngroup/lentils/service/ModelMapperWrapperTest.java
@@ -1,20 +1,48 @@
 package dk.cngroup.lentils.service;
 
+import dk.cngroup.lentils.LentilsApplication;
+import dk.cngroup.lentils.dto.CypherFormDTO;
 import dk.cngroup.lentils.dto.TeamFormDTO;
+import dk.cngroup.lentils.entity.Cypher;
 import dk.cngroup.lentils.entity.Team;
+import dk.cngroup.lentils.entity.User;
 import dk.cngroup.lentils.service.convertors.ModelMapperWrapper;
 import dk.cngroup.lentils.service.convertors.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LentilsApplication.class)
+@Transactional
 public class ModelMapperWrapperTest {
-    
+
+    private ObjectGenerator generator;
+    private ObjectMapper modelMapper;
+
+    @Autowired
+    public void setModelMapper(final ModelMapperWrapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Before
+    public void setUp() {
+        generator = new ObjectGenerator();
+    }
+
     @Test
     public void testConvertTeamToDTO() {
-        ObjectMapper modelMapper = new ModelMapperWrapper();
-        ObjectGenerator objectGenerator = new ObjectGenerator();
-        Team team = objectGenerator.generateValidTeam();
+        Team team = generator.generateValidTeam();
 
         TeamFormDTO teamFormDto = modelMapper.map(team, TeamFormDTO.class);
 
@@ -24,9 +52,7 @@ public class ModelMapperWrapperTest {
 
     @Test
     public void testUpdateTeamWithDTO() {
-        ObjectMapper modelMapper = new ModelMapperWrapper();
-        ObjectGenerator objectGenerator = new ObjectGenerator();
-        Team team = objectGenerator.generateValidTeam();
+        Team team = generator.generateValidTeam();
         TeamFormDTO teamFormDto = new TeamFormDTO();
         teamFormDto.setName("general");
         teamFormDto.setNumOfMembers(8);
@@ -35,5 +61,33 @@ public class ModelMapperWrapperTest {
 
         assertTrue(teamFormDto.getName().equals(team.getName()));
         assertTrue(teamFormDto.getNumOfMembers().equals(team.getNumOfMembers()));
+    }
+
+    @Test
+    public void testConvertCypherToDTO() {
+        Cypher cypher = generator.generateValidCypher();
+        List<User> organizers = new ArrayList<>();
+        User user = new User();
+        user.setUserId(88L);
+        organizers.add(user);
+        cypher.setOrganizers(organizers);
+
+        CypherFormDTO cypherFormDto = modelMapper.map(cypher);
+
+        assertTrue(cypher.getName().equals(cypherFormDto.getName()));
+        assertEquals(cypher.getOrganizers().get(0).getUserId(), cypherFormDto.getOrganizers().get(0));
+    }
+
+    @Test
+    public void testConvertDTOtoCypher() {
+        CypherFormDTO cypherFormDto = new CypherFormDTO();
+        List<Long> organizerIds = new ArrayList<>();
+        organizerIds.add(2L);
+        cypherFormDto.setName("fromDtoName");
+        cypherFormDto.setOrganizers(organizerIds);
+        Cypher cypher = modelMapper.map(cypherFormDto);
+
+        assertTrue(cypher.getName().equals(cypherFormDto.getName()));
+        assertEquals(cypher.getOrganizers().get(0).getUserId(), cypherFormDto.getOrganizers().get(0));
     }
 }
