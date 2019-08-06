d22fca71a0526f4a94b021f134bd2bdba4efd72e
Pavel Jurca <jurca@cngroup.dk>
2019-08-06 10:33:28 +0200

    add Tests

--- src/test/java/dk/cngroup/lentils/service/CypherServiceIntegrationTest.java
+++ src/test/java/dk/cngroup/lentils/service/CypherServiceIntegrationTest.java
@@ -3,10 +3,13 @@ package dk.cngroup.lentils.service;
 import dk.cngroup.lentils.LentilsApplication;
 import dk.cngroup.lentils.entity.Cypher;
 import dk.cngroup.lentils.entity.CypherStatus;
+import dk.cngroup.lentils.entity.Role;
 import dk.cngroup.lentils.entity.Status;
 import dk.cngroup.lentils.entity.Team;
+import dk.cngroup.lentils.entity.User;
 import dk.cngroup.lentils.repository.CypherRepository;
 import dk.cngroup.lentils.repository.HintRepository;
+import dk.cngroup.lentils.repository.RoleRepository;
 import dk.cngroup.lentils.repository.StatusRepository;
 import dk.cngroup.lentils.repository.TeamRepository;
 import dk.cngroup.lentils.repository.UserRepository;
@@ -16,6 +19,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -38,6 +46,8 @@ public class CypherServiceIntegrationTest {
     private StatusRepository statusRepository;
     @Autowired
     private HintRepository hintRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
     final ObjectGenerator objectGenerator = new ObjectGenerator();
 
@@ -89,6 +99,33 @@ public class CypherServiceIntegrationTest {
         assertEquals(second.getStage(), result.getStage());
     }
 
+    @Test
+    public void getAllNotAssignedToOrganizerTest() {
+        Cypher cypher1 = objectGenerator.generateNewCypher();
+        Cypher cypher2 = objectGenerator.generateNewCypher();
+        Cypher cypher3 = objectGenerator.generateNewCypher();
+        User assertedOrganizer = createUser("org", "pwd", "ORGANIZER");
+        User organizer = createUser("org2", "pwd2", "ORGANIZER");
+        List<User> asserted = new ArrayList<>();
+        asserted.add(assertedOrganizer);
+        List<User> organizers = new ArrayList<>();
+        asserted.add(organizer);
+        cypher1.setOrganizers(asserted);
+        cypher2.setOrganizers(organizers);
+        cypher3.setOrganizers(organizers);
+        List<Cypher> expectedCyphers = new ArrayList<>();
+        expectedCyphers.add(cypher2);
+        expectedCyphers.add(cypher3);
+
+        userRepository.save(assertedOrganizer);
+        userRepository.save(organizer);
+        cypherRepository.save(cypher1);
+        cypherRepository.save(cypher2);
+        cypherRepository.save(cypher3);
+
+        assertEquals(expectedCyphers, testedService.getAllNotAssignedToOrganizer(organizer));
+    }
+
     /**
      * This tests #65
      * Cypher with Status record could not be deleted due to incorrect FK constrains.
@@ -124,5 +161,15 @@ public class CypherServiceIntegrationTest {
         cypher.setStage(stageNumber);
 
         return cypherRepository.save(cypher);
+    }
+
+    private User createUser(String username, String password, String role) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        Set<Role> roles = new HashSet<>();
+        roles.add(roleRepository.findByName(role));
+        user.setRoles(roles);
+        return user;
     }
 }
