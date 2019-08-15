0a5c07b0b98a5871b334c991be1e42f503b75daf
Pavel Jurca <jurca@cngroup.dk>
2019-08-15 09:07:27 +0200

    use SpringRunner instead of SpringJUnit4ClassRunner

--- src/test/java/dk/cngroup/lentils/service/UserServiceIntegrationTest.java
+++ src/test/java/dk/cngroup/lentils/service/UserServiceIntegrationTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
@@ -20,7 +20,7 @@ import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = LentilsApplication.class)
 @Transactional
 public class UserServiceIntegrationTest {
