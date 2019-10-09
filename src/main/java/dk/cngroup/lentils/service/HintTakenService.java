65d7327c9832717f75fb386675d5822c4522e350
Pavel Jurca <jurca@cngroup.dk>
2019-10-09 13:57:23 +0200

    fix method getAllByTeamAndCypher in HintTakenService

--- src/main/java/dk/cngroup/lentils/service/HintTakenService.java
+++ src/main/java/dk/cngroup/lentils/service/HintTakenService.java
@@ -52,7 +52,7 @@ public class HintTakenService {
     }
 
     public List<HintTaken> getAllByTeamAndCypher(final Team team, final Cypher cypher) {
-        return hintTakenRepository.findAllByTeamAndCypher(cypher.getCypherId(), team.getTeamId());
+        return hintTakenRepository.findAllByTeamAndCypher(team.getTeamId(), cypher.getCypherId());
     }
 
     public List<HintTaken> getAll() {
